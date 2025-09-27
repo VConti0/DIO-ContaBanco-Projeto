e67a034aa1572226322f843c0e96a0d96caf6e00
VConti0 <vicenzocontioo@gmail.com>
2025-09-26 21:42:24 -0300

    primeiro-commit-projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
+        // pedindo numero da conta
         System.out.println("Digite o número de sua conta: ");
         int numeroConto = scanner.nextInt();
 
